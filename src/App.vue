3fb8a3de18d8aca869eaa32542363a61b079b109
Rubia Cacemiro <rubiarobertac@gmail.com>
2025-06-28 19:51:30 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import Body from "./components/Body.vue";
+</script>
+
+<template>
+  <Navbar />
+  <Body />
+</template>
+
+<style scoped></style>
